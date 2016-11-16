80398b567d22b5bc466c768f405c8dad1f4dabc9
Hudson Peden <hudsonpeden@gmail.com>
2016-11-15 18:46:36 -0600

    initial commit

---
+++ lib/app_component.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2016, hudsonpeden. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+import 'badge_component.dart';
+
+@Component(
+    selector: 'my-app',
+    styleUrls: const ['app_component.css'],
+    templateUrl: 'app_component.html',
+    directives: const [BadgeComponent])
+class AppComponent {}
